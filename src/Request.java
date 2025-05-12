5695362b62b148ed3a262ced0112e1cc6b86241f
jonathandavidsson <jondav03@gmail.com>
2025-05-12 15:23:37 +0200

    Added request functions to be implemented

--- src/Request.java
+++ src/Request.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class Request {
+
+    private Node originNode;
+    private Event event;
+    private int lifeTime;
+    private ArrayList<Node> path;
+
+    public Request implements Messenger(){
+
+    }
+
+    public void traverse(){
+
+    }
+
+    public Event getEvent(){
+
+    }
+
+    private boolean hasReachedEvent(){
+
+    }
 }
