2347956f3e26121eb1d7cf22ee8784ede4cc63bf
dv24jon <dv24jon@sc.umu.se>
2025-05-12 15:26:55 +0200

    Made the initial interface for Messenger.

--- src/Messenger.java
+++ src/Messenger.java
@@ -1,2 +1,23 @@
+/**
+ * An interface used by Agent and Request.
+ */
 public interface Messenger {
+
+    /**
+     * traverses in the map
+     */
+    public  void traverse();
+
+
+    /**
+     * Gets the messengers Event
+     * @return Event
+     */
+    public Event getEvent();
+
+    /**
+     * Gets the messengers Node
+     * @return Node
+     */
+    public Node getNode();
 }
