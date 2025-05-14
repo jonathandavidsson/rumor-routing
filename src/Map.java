03bd8e2c3335d76c5bb252bf0d90b38deedda288
dv24jon <dv24jon@cs.umu.se>
2025-05-14 11:18:16 +0200

    Impemented untested constructor for the map

--- src/Map.java
+++ src/Map.java
@@ -1,12 +1,48 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Map {
-    ArrayList<Node> theInfo;
-    ArrayList<Event> Events;
-    ArrayList<Node> requestNodes;
+    private ArrayList<Node> theInfo;
+    private ArrayList<Event> Events;
+    private ArrayList<Node> requestNodes;
+    private final int nodeReach = 15;
 
-    public Map() {
+    public Map(Scanner s) {
+        ArrayList<Position> positions = readMazeDataToPositions(s);
+        addNeighbours(positions);
+    }
 
+    private ArrayList<Position> readMazeDataToPositions(Scanner s){
+        String str;
+        theInfo=new ArrayList<>();
+        ArrayList<Position> positions = new ArrayList<>();
+
+        try {
+            int numberOfLines = Integer.parseInt(s.nextLine());
+            for (int i = numberOfLines; i > 0; i--){
+                str=s.nextLine();
+                String[] numbers = str.split(",");
+                int x = Integer.parseInt(numbers[0]);
+                int y = Integer.parseInt(numbers[1]);
+
+                positions.add(new Position(x, y));
+            }
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(e);
+        }
+        return positions;
+    }
+    private void addNeighbours(ArrayList<Position> positions) {
+        for (Position pos1: positions) {
+            ArrayList<Position> pos1Neigbours = new ArrayList<>();
+            for (Position pos2: positions) {
+                
+                if (!pos1.equals(pos2) && pos1.distance(pos2) <= nodeReach) {
+                    pos1Neigbours.add(pos2);
+                }
+                theInfo.add(new Node(pos1, pos1Neigbours));
+            }
+            
+        }
     }
 
     public ArrayList<Node> getMap() {
--- src/Node.java
+++ src/Node.java
@@ -1,13 +1,18 @@
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Set;
 
 public class Node {
 
     private ArrayList<Position> neighbours;
     private Position pos;
+    private Event event;
+    //Hashtable<Event, ArrayList<Object>> eventInfo;
     /*- events: Table<event: Event, direction: Position, shortest way: int*/
 
-    public Node(Position pos){
+
+    public Node(Position pos, ArrayList<Position> neighbours){
+
 
     }
 
