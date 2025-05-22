c2bef736db8c3849c713b8b3492435761d3570cf
lukas <c24lpk@cs.umu.se>
2025-05-22 16:40:48 +0200

    did getposition and getneighours test

--- test/NodeTest.java
+++ test/NodeTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NodeTest {
@@ -13,11 +14,28 @@ class NodeTest {
 
     @Test
     void getNeighbours() {
+        Position centerPos = new Position(0, 0);
+        Position neighbor1Pos = new Position(1, 0);
+        Position neighbor2Pos = new Position(0, 1);
 
+        Node center = new Node(centerPos);
+        Node neighbor1 = new Node(neighbor1Pos);
+        Node neighbor2 = new Node(neighbor2Pos);
+
+        center.addNeighbour(neighbor1);
+        center.addNeighbour(neighbor2);
+
+        ArrayList<Node> neighbours = center.getNeighbours();
+
+        assertEquals(2, neighbours.size());
+        assertTrue(neighbours.contains(neighbor1));
+        assertTrue(neighbours.contains(neighbor2));
     }
+
 
     @Test
     void getKnownEvents() {
+
     }
 
     @Test
