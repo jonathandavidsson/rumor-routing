27105c3b31ea822f83b08dcac231bb84db214615
jonathandavidsson <jondav03@gmail.com>
2025-05-28 13:43:02 +0200

    Commented Node

--- src/Node.java
+++ src/Node.java
@@ -2,6 +2,14 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Set;
 
+/**
+ * Class: Node
+ * Description: Node i a very important class to this program. Every node has a specific position in the map. Agents, events and requests
+ * can be created at a node, and agent and request uses nodes to traverse through the map.
+ *
+ * Date: 28/05/25
+ * Authors: Jonathan Davidsson, Joel ..., Liam ..., Lukasz ...
+ */
 public class Node {
 
     private ArrayList<Node> neighbours;
@@ -16,17 +24,34 @@ public class Node {
         knownEvents = new ArrayList<>();
     }
 
+    /**
+     * Description: Returns the postion of the node.
+     * @return Returns the position.
+     */
     public Position getPosition(){
         return pos;
     }
+
+    /**
+     * Returns the neighbours of the node.
+     * @return Return an arraylist containing every neighbouring node of this node.
+     */
     public ArrayList<Node> getNeighbours() {
         return neighbours;
     }
+
+    /**
+     * Description: Returns the known events in the node.
+     * @return Returns an arraylist containing every event known to the specific node.
+     */
     public ArrayList<Event> getKnownEvents() {
         return knownEvents;
     }
 
-
+    /**
+     * Description: Adds events to the nodes arraylist of known events.
+     * @param event The event to be added.
+     */
     public void setNewEvent(Event event) {
         knownEvents.add(event);
     }
@@ -61,6 +86,11 @@ public class Node {
 //            knownEvents.put(e, agentInfo);
 //        }
 //    }
+
+    /**
+     * Description: Adds a neighbour to the nodes arraylist of neighbours.
+     * @param node the node to be added to the neighbours arraylist.
+     */
     public void addNeighbour(Node node){
         neighbours.add(node);
     }
