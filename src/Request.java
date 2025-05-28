e40867218d89b0a059c93633cea27922725423ce
dv24jon <dv24jon@cs.umu.se>
2025-05-28 14:07:52 +0200

    Added comments to the request.

--- src/Request.java
+++ src/Request.java
@@ -1,6 +1,14 @@
 import java.util.ArrayList;
 import java.util.Stack;
 
+/**
+ * Request is a class that navigates in the class map. The request is created at a specified node in
+ * the map. The purpose of the request is to find the origin of an event and carry the info from that event back to
+ * the origin of the Request.
+ *
+ * Date: 28/05/25
+ * @author: Jonathan Davidsson, Joel Lindgren, Liam ..., Lukasz ...
+ */
 public class Request {
 
     private final Node originNode;
@@ -22,9 +30,13 @@ public class Request {
     }
 
     /**
-     * first look for event in node,
-     * else look if the node knows a event to the node
-     * else takes a random path.
+     * This function tries to traverse in the Map by, looking if it stands on the eventNode,
+     * otherwise it looks if the node it stands on knows a path to the event, otherwise it takes a random path.
+     * If the request is dead it throws and exception, and if it has found the originNode it goes back.
+     * If the function hasnt found the node the event Originated from in 45 steps, it dies.
+     * @throws IsDeadException If the request is dead and traverse is called on it.
+     * @implNote the request doesn't lose life when traversing back to the originNode
+     * @return true if the request is back at its OriginNode
      */
     public boolean traverse(){
 
@@ -59,21 +71,38 @@ public class Request {
         return false;
     }
 
-    /**
-     * Returns true
-     * @return
+
+    /*
+     * Checks if the request are on the same position as the originNode.
+     * @return true if the requests position are the same as where the request spawned.
      */
     private boolean hasReachedOriginNode(){
         return currentNode.equals(originNode);
     }
 
+    /*
+     * Traverses back, takes one step back to where it came, following its own path.
+     */
+    private void traverseBackToNode(){
+        if (!path.empty()) {
+            path.pop();
+            if (!path.isEmpty()) {
+                currentNode = path.peek();
+            }
+        }
+    }
+
+    /*
+     * Try to follow nodes that knows where the event, the request is trying to find, is.
+     * @return if the request takes a step the function returns true, otherwise it returns false.
+     */
     private boolean followAPathToEvent() {
         ArrayList<Event> knownEvents = currentNode.getKnownEvents();
         if (!knownEvents.isEmpty())
         {
             Event matchingEvent = null;
             for (Event knownEvent: knownEvents) {
-                //if the node request is on knows a path to a event then this function follows it.
+                //if the node request is on a path to an event then this function follows it.
                 if (knownEvent.equals(this.event)){
                     matchingEvent = knownEvent;
                     break;
@@ -83,10 +112,6 @@ public class Request {
             if(matchingEvent != null && matchingEvent.getNodeToEvent() != null){
                 path.push(matchingEvent.getNodeToEvent());
                 currentNode = matchingEvent.getNodeToEvent();
-//                System.out.println("Follow path to event");
-//                System.out.println("found in(" + getCurrentNode().getPosition().toString() +
-//                        ")EventID" + matchingEvent.getEventId() + " - pathToEvent: " +
-//                        matchingEvent.getNodeToEvent().getPosition().toString());
                 lifeTime--;
                 return true;
             }
@@ -94,18 +119,21 @@ public class Request {
         return false;
     }
 
-    private void traverseBackToNode(){
-        if (!path.empty()) {
-            path.pop();
-            if (!path.isEmpty()) {
-                currentNode = path.peek();
-            }
-        }
-    }
+
+
+    /*
+     * This function is spiritual, but simulates the request taking info from the origin of an event,
+     * but because the Request already knows the whole event, nothing happens.
+     */
     private void getEventInNode(){
-        //TODO nothing needs to be done, because the request already knows the event.
+        //Does nothing
     }
 
+    /*
+     * trys to move to a random neighbour to the current node. Tries to move to a node the request hasn't been on yet,
+     * but can move to such a node otherwise.
+     * If it cant, the request dies.
+     */
     private void moveToRandomNode(){
         ArrayList<Node> movableNeighbours = getMovableNeighbours(getCurrentNode().getNeighbours());
 
@@ -115,15 +143,27 @@ public class Request {
             currentNode = movableNeighbours.get(newNode);
         }
         else {
-            lifeTime = 0; //The request dies if it gets stuck
+            ArrayList<Node> neighbours = getCurrentNode().getNeighbours();
+
+            if (!neighbours.isEmpty()){
+                int newNode = (int)(neighbours.size() * Math.random());
+                path.push(neighbours.get(newNode));
+                currentNode = neighbours.get(newNode);
+            }
+            else {
+                lifeTime = 0; //The request dies if it gets stuck
+            }
         }
 
     }
 
-    public Node getCurrentNode() {
-        return currentNode;
-    }
 
+    /*
+     * This function returns the movable neighbours from a set of neighbours from a node.
+     * @implNote Agent has almost the same private function.
+     * @param neighbours of a node
+     * @return Movable Neighbours
+     */
     private ArrayList<Node> getMovableNeighbours(ArrayList<Node> neighbours){
         ArrayList<Node> movableNodes = new ArrayList<>();
 
@@ -135,6 +175,11 @@ public class Request {
         return movableNodes;
     }
 
+    /*
+     * returns true if the request are on the same node as the orgin of the event the request
+     * searches for.
+     * @return true if request are on the same node as the orgin of a event.
+     */
     private boolean hasReachedEvent(){
         if (currentNode.equals(event.getEventNode())) {
             return true;
@@ -148,16 +193,38 @@ public class Request {
         return false;
     }
 
+    /**
+     * @return the event the request searches for.
+     */
     public Event getEvent(){
         return event;
     }
+
+    /**
+     * @return the node the request was created on.
+     */
     public Node getOrginNode(){
         return originNode;
     }
+
+    /**
+     * @return The node the request is on.
+     */
+    public Node getCurrentNode() {
+        return currentNode;
+    }
+
+    /**
+     * @return true if the request is dead, otherwise it returns false.
+     */
     public boolean isDead(){
         return lifeTime <= 0;
     }
 
+
+    /**
+     * Prints the eventInfo of a request and the requests originNode.
+     */
     public void printRequestEvent(){
         System.out.println("RequestMessage from Node: " + getOrginNode().getPosition().toString() +
                 " found:" +
